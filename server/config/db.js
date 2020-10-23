e657213ebc21fd0083e7aef96b6babc8b63f627a
Gurkirat Khaira <777gurkirat@gmail.com>
2020-10-22 20:17:39 -0400

    added db link

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,7 @@
 module.exports =
 {
     //"URI": "mongodb://localhost/book_store"
-    //"URI": "mongodb+srv://khaira:EObMCIAJJYBDUREK@mongodbdatabaseserver.gchnp.mongodb.net/book_store?retryWrites=true&w=majority"
+    "URI": "mongodb+srv://gurkiratkhaira:V2BSPPUTe0FpxOfG@databaseforherokuassign.hpq66.mongodb.net/<dbname>?retryWrites=true&w=majority"
 }
+
+
